3fbb4ab5353e8712fbb2e4a2c8b908b65d39ad43
mohitbhatt394 <157204942+mohitbhatt394@users.noreply.github.com>
2024-01-20 11:45:18 +0000

    variable commited

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,45 @@
+const uniqueId = 1516;
+let emailId = 'mohitbhatt@gamil.com';
+var accountPassward = '1qaz2wsx';
+address = 'jaipur'; // it's a bad practice to declare
+let newThings; //result will be undefined
+
+// uniqueId = 1718; not allowed
+emailId = 'mb@gmail.com';
+accountPassward = '123qwe';
+address = 'haridwar';
+
+console.log(emailId);
+console.log(accountPassward);
+console.log(address);
+
+console.table([emailId,accountPassward,address,newThings]);
+
+//extras
+
+// console.info('This is an informational message');
+
+// console.warn('This is a warning message');
+
+// console.error('This is an error message');
+
+// console.clear();
+
+// console.group('Group 1');
+// console.log('Message 1');
+// console.log('Message 2');
+// console.groupEnd();
+
+// console.time('Timer');
+// console.log('tell the time between execution of code');
+// console.timeEnd('Timer');
+
+// for (let i = 0; i < 5; i++) {
+//     console.count('Loop Iteration');
+// }
+
+// console.assert(1 === 2, 'This assertion will fail');
+
+// const obj = { name: 'John', age: 30 };
+// console.dir(obj);
+
