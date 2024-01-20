a431eb1cc74595f8e437742b34c8d71af6379d87
mohitbhatt394 <157204942+mohitbhatt394@users.noreply.github.com>
2024-01-20 16:12:52 +0000

    datatypes commit

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat code as a newer version of JS code
+
+// premitive datatypes
+let name = 'mohit' //string datatype ('')
+let age = 18 //number datatype 
+let value = true //boolean datatype (true/false)
+let bignum = BigInt(14677755) //bigint datatype, to store big value (bigint/165.....n)
+let nullval = null //null datatype
+let empty; //undefined
+let symbol = Symbol() //symbol datatype, for uniqueness
+
+console.log(typeof age); //to check what datatypes it is
+console.log(typeof undefined) //output is undefined
+console.log(typeof null) //output is objects
+
+// Objects datatypes we will learn
