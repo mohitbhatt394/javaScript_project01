947f7f75d4fa4289380330aa15c8ffa35e8571ad
mohitbhatt394 <157204942+mohitbhatt394@users.noreply.github.com>
2024-01-21 05:47:11 +0000

    conversion

---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,31 @@
+let value = 3;
+let negval = -value;
+console.log(negval);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = 'mohit';
+let str2 = ' bhatt';
+console.log(str1 + str2);
+
+console.log('1' + 2);
+console.log(1 + '2');
+console.log('1' + '2');
+console.log('1' + 2 + 3);
+
+//prefix increment
+let x = 5;
+let y = ++x;
+console.log(`x:${x} y:${y}`);
+
+//postfix increment
+let x1 = 3;
+let y1 = x1++
+console.log(`x1:${x1} y1:${y1}`);
+
+
